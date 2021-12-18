35cbb2544be6c8190e5d063ba64c6e6ad16f335d
ali <aliburak.yilmaz@outlook.com>
2021-12-19 00:24:15 +0300

    add main content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import MainContent from "./MainContent";
 import NavBar from "./NavBar";
+
+
 export default class App extends 
 Component{
  render(){
-     return<NavBar>Hello</NavBar>;
+     return( 
+    <React.Fragment>
+     <NavBar/>
+     <MainContent/>
+     </React.Fragment>
+     );
  }
 }
 
