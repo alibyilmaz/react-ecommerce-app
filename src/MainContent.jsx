0a6d9c618d10a5c1711ac4cc07758512bcd60ee7
ali <aliburak.yilmaz@outlook.com>
2021-12-19 01:10:47 +0300

    add customer table

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -2,7 +2,15 @@
 import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 export default class MainContent extends Component {
-state = {pageTitle:"Customers", customersCount:5};
+state = {pageTitle:"Customers", customersCount:5,
+customers:[
+    {id:1,name:"Scott",phone:"123-456"},
+    {id:2,name:"Jones",phone:"982-014"},
+    {id:3,name:"Allen",phone:"889-921"},
+    {id:4,name:"James",phone:"552-901"},
+    {id:5,name:"John",phone:"781-778"},
+],
+};
 
 render() {
             return (
@@ -14,12 +22,37 @@ render() {
                   </span>
                   <button className="btn btn-info" onClick={this.onRefreshClick}>Refresh</button>
                   </h4>
+                  <table className="table">
+                   <thead>
+                       <tr>
+                           <th>#</th>
+                           <th>Customer Name</th>
+                           <th>Phone</th>
+                       </tr>
+                   </thead>
+                   <tbody>
+                       {
+                           this.state.customers.map((cust)=>{
+                               return(
+                                <tr key={cust.id}>
+                                <td>{cust.id}</td>
+                                <td>{cust.name}</td>
+                                <td>{cust.phone}</td>
+                            </tr>
+                               );
+                           })
+                       }
+
+                   </tbody>    
+                  </table>
                 </div>
             );
         }
 
-onRefreshClick()
+onRefreshClick = () =>
 {
-console.log("refresh clicked")
+    this.setState({
+    customersCount:7
+})
 }
     }
